15bc42c24b393fa892d0e87e70457b195522fb62
Md Rakib Hossain <rakib.hossain@selise.ch>
2022-12-19 10:24:27 +0600

    Event by year complete

--- src/Components/FilterTypeCustomDate/FilterTypeCustomDate.js
+++ src/Components/FilterTypeCustomDate/FilterTypeCustomDate.js
@@ -1,12 +1,19 @@
 import { DatePicker } from 'antd';
-import React, { useState } from 'react';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import deleteIcon from '../../images/delete.svg';
 import plusIcon from '../../images/plus.svg';
+import { setEventbyYearTimelineMonth } from '../../Redux/Slice/EventByYear/eventByYearTimelineMonth';
+import { setEventbyYearTimelineYears } from '../../Redux/Slice/EventByYear/eventByYearTimelineYear';
 import './FilterTypeCustomDate.scss';
 
 function FilterTypeCustomDate() {
+    const location = useSelector((state) => state.location.value);
+
     const [dateRanges, setDateRanges] = useState([{ from: null, to: null }]);
     const assetsPath = window.eluxDashboard.assetsUrl;
+    const dispatch = useDispatch();
 
     const addDateRange = () => {
         setDateRanges([...dateRanges, { from: null, to: null }]);
@@ -27,54 +34,79 @@ function FilterTypeCustomDate() {
         newDateRanges[index].to = date;
         setDateRanges(newDateRanges);
     };
-    const commonSelectedYears = () => {
-        const selectedYears = dateRanges.map((dateRange) => {
-            const fromYear = dateRange.from ? dateRange.from.getFullYear() : null;
-            const toYear = dateRange.to ? dateRange.to.getFullYear() : null;
-            return [fromYear, toYear];
-        });
+    // const commonSelectedYears = () => {
+    //     const selectedYears = dateRanges.map((dateRange) => {
+    //         const fromYear = dateRange.from ? dateRange.from.getFullYear() : null;
+    //         const toYear = dateRange.to ? dateRange.to.getFullYear() : null;
+    //         return [fromYear, toYear];
+    //     });
 
-        const commonYears = selectedYears.reduce((acc, years) => {
-            const [fromYear, toYear] = years;
-            if (fromYear && toYear) {
-                for (let year = fromYear; year <= toYear; year += 1) {
-                    acc[year] = (acc[year] || 0) + 1;
-                }
-            } else if (fromYear) {
-                acc[fromYear] = (acc[fromYear] || 0) + 1;
-            } else if (toYear) {
-                acc[toYear] = (acc[toYear] || 0) + 1;
-            }
-            return acc;
-        }, {});
+    //     const commonYears = selectedYears.reduce((acc, years) => {
+    //         const [fromYear, toYear] = years;
+    //         if (fromYear && toYear) {
+    //             for (let year = fromYear; year <= toYear; year += 1) {
+    //                 acc[year] = (acc[year] || 0) + 1;
+    //             }
+    //         } else if (fromYear) {
+    //             acc[fromYear] = (acc[fromYear] || 0) + 1;
+    //         } else if (toYear) {
+    //             acc[toYear] = (acc[toYear] || 0) + 1;
+    //         }
+    //         return acc;
+    //     }, {});
 
-        return Object.keys(commonYears).filter((year) => commonYears[year] === dateRanges.length);
-    };
-    const commonSelectedMonths = () => {
-        const selectedMonths = dateRanges.map((dateRange) => {
-            const fromMonth = dateRange.from ? dateRange.from.getMonth() : null;
-            const toMonth = dateRange.to ? dateRange.to.getMonth() : null;
-            return [fromMonth, toMonth];
-        });
+    //     return Object.keys(commonYears).filter((year) => commonYears[year] === dateRanges.length);
+    // };
+    // const commonSelectedMonths = () => {
+    //     const selectedMonths = dateRanges.map((dateRange) => {
+    //         const fromMonth = dateRange.from ? dateRange.from.getMonth() : null;
+    //         const toMonth = dateRange.to ? dateRange.to.getMonth() : null;
+    //         return [fromMonth, toMonth];
+    //     });
 
-        const commonMonths = selectedMonths.reduce((acc, months) => {
-            const [fromMonth, toMonth] = months;
-            if (fromMonth && toMonth) {
-                for (let month = fromMonth; month <= toMonth; month += 1) {
-                    acc[month] = (acc[month] || 0) + 1;
-                }
-            } else if (fromMonth) {
-                acc[fromMonth] = (acc[fromMonth] || 0) + 1;
-            } else if (toMonth) {
-                acc[toMonth] = (acc[toMonth] || 0) + 1;
-            }
-            return acc;
-        }, {});
+    //     const commonMonths = selectedMonths.reduce((acc, months) => {
+    //         const [fromMonth, toMonth] = months;
+    //         if (fromMonth && toMonth) {
+    //             for (let month = fromMonth; month <= toMonth; month += 1) {
+    //                 acc[month] = (acc[month] || 0) + 1;
+    //             }
+    //         } else if (fromMonth) {
+    //             acc[fromMonth] = (acc[fromMonth] || 0) + 1;
+    //         } else if (toMonth) {
+    //             acc[toMonth] = (acc[toMonth] || 0) + 1;
+    //         }
+    //         return acc;
+    //     }, {});
 
-        return Object.keys(commonMonths).filter(
-            (month) => commonMonths[month] === dateRanges.length
-        );
-    };
+    //     return Object.keys(commonMonths).filter(
+    //         (month) => commonMonths[month] === dateRanges.length
+    //     );
+    // };
+
+    const yearsArray = dateRanges.reduce((years, dateRange) => {
+        // Extract the years from the "from" and "to" fields
+        const fromYear = moment(dateRange.from).year();
+        const toYear = moment(dateRange.to).year();
+
+        // Add the years to the array
+        years.push(fromYear, toYear);
+
+        return years;
+    }, []);
+
+    // console.log([...new Set(yearsArray)]);
+    useEffect(() => {
+        switch (location) {
+            case 'event-by-year-timeline':
+                dispatch(setEventbyYearTimelineYears([...new Set(yearsArray)]));
+                dispatch(setEventbyYearTimelineMonth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+
+                break;
+
+            default:
+                console.log('filter type year month default');
+        }
+    }, [dateRanges]);
 
     return (
         <div className="custom-date-range-wrapper">
--- src/Components/FilterTypeTImeFrame/FilterTypeTimeFrame.js
+++ src/Components/FilterTypeTImeFrame/FilterTypeTimeFrame.js
@@ -1,17 +1,99 @@
 import { Select } from 'antd';
-import React from 'react';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setEventbyYearTimelineMonth } from '../../Redux/Slice/EventByYear/eventByYearTimelineMonth';
+import { setEventbyYearTimelineYears } from '../../Redux/Slice/EventByYear/eventByYearTimelineYear';
 import './FilterTypeTimeFrame.scss';
 
 function FilterTypeTimeFrame() {
+    const [time, setTime] = useState('1');
+    const [frame, setFrame] = useState('years');
+    const location = useSelector((state) => state.location.value);
+
+    const dispatch = useDispatch();
+
     const handleTimeChange = (value) => {
-        console.log(value);
+        setTime(value);
     };
+
+    const handleFrmaeChange = (value) => {
+        setFrame(value);
+    };
+
+    const getYears = (value, lastYears) => {
+        let now = moment().year();
+
+        for (let i = 0; i < value; i += 1) {
+            const lastYear = now - 1;
+
+            lastYears.push(lastYear);
+
+            now = lastYear;
+        }
+    };
+
+    const getMonths = (value, years) => {
+        const now = moment();
+        const currentYear = now.year();
+
+        const lastMonths = [];
+        const numYears = Math.floor(value / 12);
+        const monthsByYear = [];
+        for (let i = 0; i < value; i += 1) {
+            const lastMonth = now.subtract(1, 'month');
+
+            lastMonths.push(lastMonth);
+        }
+
+        lastMonths.forEach((month) => {
+            const year = month.year();
+            if (!monthsByYear[year]) {
+                monthsByYear[year] = [];
+            }
+            monthsByYear[year].push(month);
+        });
+        if (currentYear in monthsByYear) {
+            monthsByYear[currentYear].push(now);
+        }
+
+        for (let year = currentYear; year >= currentYear - numYears; year -= 1) {
+            if (year in monthsByYear) {
+                years.push(year);
+            } else {
+                years.push(year);
+                monthsByYear[year] = [];
+            }
+        }
+    };
+
+    useEffect(() => {
+        const lastYears = [];
+        const years = [];
+
+        switch (location) {
+            case 'event-by-year-timeline':
+                if (frame === 'years') {
+                    getYears(time, lastYears);
+                    dispatch(setEventbyYearTimelineYears(lastYears));
+                    dispatch(setEventbyYearTimelineMonth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+                } else {
+                    getMonths(time, years);
+                    dispatch(setEventbyYearTimelineYears(years));
+                    dispatch(setEventbyYearTimelineMonth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+                }
+                break;
+
+            default:
+                console.log('filter type year month default');
+        }
+    }, [time, frame]);
     return (
         <div className="filter-type-time-frame">
             <span>Last</span>
             <Select
                 className="time-frame-filter"
-                defaultValue="1"
+                defaultValue={time}
                 style={{
                     width: '100%',
                 }}
@@ -25,15 +107,19 @@ function FilterTypeTimeFrame() {
                         value: '2',
                         label: '2',
                     },
+                    {
+                        value: '50',
+                        label: '50',
+                    },
                 ]}
             />
             <Select
                 className="time-frame-filter"
-                defaultValue="years"
+                defaultValue={frame}
                 style={{
                     width: '100%',
                 }}
-                onChange={handleTimeChange}
+                onChange={handleFrmaeChange}
                 options={[
                     {
                         value: 'months',
