b7b88d289673cc1d20d65a60249f3b37660532e2
arnab-selise <arnab.mondal@selise.com>
2022-12-19 13:12:56 +0600

    graph view 50% done

--- src/Components/EventByLocation/EventByLocation.js
+++ src/Components/EventByLocation/EventByLocation.js
@@ -17,113 +17,118 @@ import GraphTableSwitch from '../GraphTableSwitch/GraphTableSwitch';
 import LocalFilter from '../LocalFilter/LocalFilter';
 import './EventByLocation.scss';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const options = {
-    plugins: {
-        title: {
-            display: false,
-        },
+  plugins: {
+    title: {
+      display: false,
     },
-    responsive: true,
-    scales: {
-        x: {
-            stacked: true,
-        },
-        y: {
-            stacked: true,
-        },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
     },
+    y: {
+      stacked: true,
+    },
+  },
 };
 const { Column } = Table;
 
 function EventByLocation() {
-    const [requestData, setRequestData] = useState('events');
-    console.log('re', requestData);
+  const [requestData, setRequestData] = useState('events');
+  console.log('re', requestData);
 
-    const [grapOrTableForLocation, setgGrapOrTableForLocation] = useState('graph');
-    const handleSwitchChange = (e) => {
-        setgGrapOrTableForLocation(e.target.value);
-        console.log(e.target.value);
-    };
-    const payload = {
-        request_data: 'events',
-        filter_type: 'months',
-        request_body: JSON.stringify([
-            {
-                "year": "2012",
-                "months": "01"
-            },
-             {
-                "year": "2021",
-                "months": "01"
-            }
-        ]),
-    };
+  const [grapOrTableForLocation, setgGrapOrTableForLocation] =
+    useState('graph');
+  const handleSwitchChange = (e) => {
+    setgGrapOrTableForLocation(e.target.value);
+    console.log(e.target.value);
+  };
+  const payload = {
+    request_data: 'events',
+    filter_type: 'months',
+    locations: '459,188',
+    request_body: JSON.stringify([
+      {
+        year: '2022',
+        months: '02,08',
+      },
+      {
+        year: '2021',
+        months: '03,06',
+      },
+      {
+        year: '2020',
+        months: '04,09',
+      },
+    ]),
+  };
 
-    const { data } = useEventByLocationQuery(payload);
-    // console.log('data', data);
-    const labels =
-        data && data.data.years.map((year) => year.year)
-            ? data.data.years.map((year) => year.year)
-            : ['2022'];
-    const graphData = {
-        labels,
-        datasets: [
-            {
-                label: 'ELUX',
-                data:
-                    data && data.data.years.map((year) => year.elux)
-                        ? data.data.years.map((year) => year.elux)
-                        : 0,
-                backgroundColor: '#4A2017',
-            },
-            {
-                label: 'B2B',
-                data:
-                    data && data.data.years.map((year) => year.b2b)
-                        ? data.data.years.map((year) => year.b2b)
-                        : 0,
-                backgroundColor: '#937359',
-            },
-            {
-                label: 'B2C',
-                data:
-                    data && data.data.years.map((year) => year.b2c)
-                        ? data.data.years.map((year) => year.b2c)
-                        : 0,
-                backgroundColor: '#D0B993',
-            },
-        ],
+  const { data } = useEventByLocationQuery(payload);
+  // console.log('data', data);
+
+  const getYears = (data) => {
+    return [2022, 2021, 2020];
+  };
+
+  const colors = ['#93735a', '#4a2016', '#a6b2a4', '#6b7a66', '#3b4536' ]
+  const labels = getYears(data);
+  const datasets = labels.map((label , index) => {
+    return {
+      label: label,
+      data:
+        (data && data.data.locations.map((location) => location.label)) || 0,
+      backgroundColor: colors[index % colors.length],
     };
-    return (
-        <>
-            <div className="header-wrapper">
-                <GraphTableSwitch
-                    grapOrTable={grapOrTableForLocation}
-                    handleSwitchChange={handleSwitchChange}
-                />
-                <DownloadButton />
-            </div>
-            <div className="graph-overview">
-                <h2 className="graph-title">
-                    Overview of Events per location <span>by year</span>
-                </h2>
-            </div>
-            <LocalFilter requestData={requestData} setRequestData={setRequestData} displayClass={"d-block"}/>
-            {grapOrTableForLocation === 'graph' ? (
-                <Bar options={options} data={graphData} />
-            ) : (
-                <Table dataSource={data.data.years}>
-                    <Column title="Year" dataIndex="year" key="year" />
-                    <Column title="ELUX" dataIndex="elux" key="elux" />
-                    <Column title="B2B" dataIndex="b2b" key="b2b" />
-                    <Column title="B2C" dataIndex="b2c" key="b2c" />
-                    <Column title="Total" dataIndex="total" key="total" />
-                </Table>
-            )}
-        </>
-    );
+  });
+  // console.log('labels', labels);
+  // console.log('datasets', datasets);
+  const graphData = {
+    labels,
+    datasets: datasets,
+  };
+  return (
+    <>
+      <div className="header-wrapper">
+        <GraphTableSwitch
+          grapOrTable={grapOrTableForLocation}
+          handleSwitchChange={handleSwitchChange}
+        />
+        <DownloadButton />
+      </div>
+      <div className="graph-overview">
+        <h2 className="graph-title">
+          Overview of Events per location <span>by year</span>
+        </h2>
+      </div>
+      <LocalFilter
+        requestData={requestData}
+        setRequestData={setRequestData}
+        displayClass={'d-block'}
+      />
+      {grapOrTableForLocation === 'graph' ? (
+        <Bar options={options} data={graphData} />
+      ) : (
+        <Table dataSource={data.data.years}>
+          <Column title="Year" dataIndex="year" key="year" />
+          <Column title="ELUX" dataIndex="elux" key="elux" />
+          <Column title="B2B" dataIndex="b2b" key="b2b" />
+          <Column title="B2C" dataIndex="b2c" key="b2c" />
+          <Column title="Total" dataIndex="total" key="total" />
+        </Table>
+      )}
+    </>
+  );
 }
 
 export default EventByLocation;
