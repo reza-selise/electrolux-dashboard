d18a8dd573a3969aaf232d22ed2afe9bf4f97f9c
coder618selise <ahadul.islam@selise.ch>
2022-12-26 15:54:16 +0600

    Table data format added

--- inc/events/events-by-cooking-course-type-api.php
+++ inc/events/events-by-cooking-course-type-api.php
@@ -66,15 +66,20 @@ if( ! function_exists( 'elux_get_events_by_cooking_course_type' ) ){
 
         
         // /// 4. ---------- Get Final output
-        $final_data         = el_events_by_cooking_course_type_FINAL_DATA($filtered_data, $received_data);
+        $graph_data         = el_events_by_cooking_course_type_FINAL_DATA($filtered_data, $received_data);
+
+        $table_data         = el_event_by_cooking_course_TABLE_FINAL_DATA($filtered_data, $received_data);
+
+        // print_r($table_data);
 
 
-
-        if($structure_data && $final_data ){
+        if($structure_data && $graph_data  && $table_data ){
             return rest_ensure_response( array(
                 'status'    => true,
                 'message'   => 'Data fetch successful',
-                'data'      => $final_data,
+                'data'      => $graph_data,
+                'table_data'=> $table_data,
+                'graph_data'=> $graph_data
             ) );
         }else{
             return rest_ensure_response( array(
@@ -304,4 +309,144 @@ function el_events_by_cooking_course_type_FINAL_DATA($structure_data, $requestDa
     ];
 
 
+}
+
+
+
+function el_event_by_cooking_course_TABLE_FINAL_DATA($structure_data, $requestData){
+    
+    $labels = []; // store all the years
+
+
+    foreach($structure_data  as $id =>  $item){
+        $year = $item['event_time']['year'];
+        if(!in_array($year, $labels)){
+            $labels[] = $year;
+        }
+    }
+
+
+    /*
+
+        [cat_id] =>{
+            label : category name,
+            year_data : {
+                2022 : 5
+                2021 : 5
+            }
+        }
+
+        [658] =>{
+            label : Fish,
+            total : 6546546
+            year_data : {
+                2022 : 54
+                2021 : 589
+            }
+        }
+
+    */
+
+    // count the data 
+    $data_by_category_and_year = [];
+    
+    foreach($structure_data  as $id =>  $product_data){
+
+
+
+        $category_list  = $product_data['category'];
+        $event_year  = $product_data['event_time']['year'];
+
+        foreach($category_list as $cat_id => $cat_name){
+
+            if( !isset($data_by_category_and_year[$cat_id]) ){
+                $data_by_category_and_year[$cat_id]['label'] = $cat_name;
+            }
+
+            $previous_count = 0;
+
+            if( $requestData['type'] == 'participants' ){
+                
+
+                if( isset($data_by_category_and_year[$cat_id]['year_data'][$event_year])) {
+
+                    $previous_count = $data_by_category_and_year[$cat_id]['year_data'][$event_year];
+                }else{
+
+                }
+
+
+                
+            }else{
+
+                if( isset($data_by_category_and_year[$cat_id]['year_data'][$event_year])) {
+                    $previous_count = $data_by_category_and_year[$cat_id]['year_data'][$event_year];
+                }
+    
+                $data_by_category_and_year[$cat_id]['year_data'][$event_year] = $previous_count + 1;
+            }
+
+
+            
+
+        }
+        
+    }
+
+    // Calculate total 
+    foreach($data_by_category_and_year as $cat_id => $item){
+        $total = 0; 
+
+        $year_data = $data_by_category_and_year[$cat_id]['year_data'];
+
+        foreach($year_data as $year => $year_value ){
+            $total = $total + intval($year_value);
+        }
+
+        $data_by_category_and_year[$cat_id]['total'] = $total;
+
+    }
+
+
+
+
+    $table_rows = [];
+
+    // print_r($data_by_category_and_year);
+    foreach($data_by_category_and_year as $cat_id => $cat_arr ){
+
+
+        $each_table_row = [ $cat_arr['label'] ];
+
+
+        // loop through all the label and append the data RESPECTIVELY (in order)
+        foreach( $labels as $year ){
+
+            
+            // check if year data exist in the array or not 
+            if( isset( $cat_arr['year_data'][$year] ) ){
+                $number_to_push = $cat_arr['year_data'][$year];
+            }else{
+                $number_to_push = 0;
+            }
+
+            $each_table_row[] = $number_to_push;
+
+        }
+        // add last item 
+        $each_table_row[] = $cat_arr['total'];
+
+        $table_rows[] = $each_table_row;
+
+    }
+
+    
+    $labels[] = "Total";
+    array_unshift($labels , "Years");
+
+
+    return [
+        'labels'    => $labels,
+        'rows'      => $table_rows
+    ];
 }
