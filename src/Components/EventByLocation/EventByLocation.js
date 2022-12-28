8c62abb1cc700d5e4f3658898644a64a1f33afdf
Arnab Selise <arnab.mondal@selise.ch>
2022-12-28 15:06:51 +0600

    graph done without filter

--- src/Components/EventByLocation/EventByLocation.js
+++ src/Components/EventByLocation/EventByLocation.js
@@ -1,8 +1,53 @@
 // eslint-disable-next-line import/no-unresolved
-import { useSelector } from 'react-redux';
-import { useEventByLocationQuery } from '../../API/apiSlice';
+import {Table} from 'antd';
+import {
+    BarElement,
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LinearScale,
+    Title,
+    Tooltip
+} from 'chart.js';
+import React,{useState} from 'react';
+import {useSelector} from 'react-redux';
+// eslint-disable-next-line import/no-unresolved
+import {Bar} from 'react-chartjs-2';
+import {eluxTranslation} from '../../Translation/Translation';
+import DownloadButton from '../DownloadButton/DownloadButton';
+import GraphTableSwitch from '../GraphTableSwitch/GraphTableSwitch';
+import LocalFilter from '../LocalFilter/LocalFilter';
+
+import {useEventByLocationQuery} from '../../API/apiSlice';
 import './EventByLocation.scss';
 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const BarOptions = {
+    plugins: {
+        title: {
+            display: false,
+        },
+        legend: {
+            align: 'start',
+            labels: {
+                boxWidth: 16,
+                boxHeight: 16,
+            },
+        },
+    },
+    responsive: true,
+    scales: {
+        x: {
+            stacked: true,
+        },
+        y: {
+            stacked: true,
+        },
+    },
+    barPercentage: 1,
+};
+const { Column } = Table;
 function EventByLocation() {
     const eventByLocationFilterType = useSelector(state => state.eventByLocationFilterType.value);
     const eventByLocationTimelineYears = useSelector(
@@ -11,21 +56,85 @@ function EventByLocation() {
     const eventbyLocationTimelineMonth = useSelector(
         state => state.eventbyLocationTimelineMonth.value
     );
+
+    const [grapOrTableForLocation, setgGrapOrTableForLocation] = useState('graph');
+    const [requestDataForLocation, setRequestDataForLocation] = useState('events');
+
     const requestBody = eventByLocationTimelineYears.map(year => ({
         year: year.toString(),
         months: eventbyLocationTimelineMonth.toString(),
     }));
+    const getLocations = () => ['191', '188', '501', '502', '504'];
 
     const payload = {
         filter_type: eventByLocationFilterType,
         request_data: 'events',
-        locations: '188,191',
+        locations: getLocations().toString(),
         request_body: JSON.stringify(requestBody),
     };
     const { data, error, isLoading } = useEventByLocationQuery(payload);
-    console.log(data, error, isLoading);
+    console.log('data-location :', data, error, isLoading);
+    const getDataFromLocation = (location, locationYear) =>
+        eventByLocationTimelineYears.map(year => (locationYear === year ? location[year] : 0));
 
-    return 'Event by location';
+    const colors = ['#93735a', '#4a2016', '#a6b2a4', '#6b7a66', '#3b4536'];
+
+    const labels = getLocations();
+    const datasets = eventByLocationTimelineYears.map((year, index) => ({
+        label: year,
+        data: data && data.data.locations.map(location => getDataFromLocation(location, year)),
+        backgroundColor: colors[index % colors.length],
+    }));
+    const graphData = {
+        labels,
+        datasets,
+    };
+    console.log('data-location-dataset:', datasets);
+
+    const { pleaseWait } = eluxTranslation;
+    return (
+        <>
+            <div className="header-wrapper">
+                <GraphTableSwitch
+                    identifier={111}
+                    grapOrTable={grapOrTableForLocation}
+                    setgGrapOrTable={setgGrapOrTableForLocation}
+                    name="event-by-location"
+                />
+                <DownloadButton />
+            </div>
+            <div className="graph-overview location-graph">
+                <h2 className="graph-title">
+                    Overview of Events per location <span>by year</span>
+                </h2>
+                <LocalFilter
+                    requestData={requestDataForLocation}
+                    setRequestData={setRequestDataForLocation}
+                    location="event-by-year-timeline"
+                />
+            </div>
+
+            {error ? (
+                'error'
+            ) : isLoading ? (
+                pleaseWait
+            ) : grapOrTableForLocation === 'graph' ? (
+                <Bar options={BarOptions} data={graphData} />
+            ) : (
+                <Table
+                    dataSource={data.data.years}
+                    pagination={false}
+                    className="event-by-location-table"
+                >
+                    <Column title="Year" dataIndex="year" key="year" />
+                    <Column title="ELUX" dataIndex="elux" key="elux" />
+                    <Column title="B2B" dataIndex="b2b" key="b2b" />
+                    <Column title="B2C" dataIndex="b2c" key="b2c" />
+                    <Column title="Total" dataIndex="total" key="total" />
+                </Table>
+            )}
+        </>
+    );
 }
 
 export default EventByLocation;
