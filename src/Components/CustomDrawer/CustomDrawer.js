e5b1d147973ded46cc712247206f06a9fcdadfb9
Md Asifuzzaman Suvo <asifuzzaman.suvo@selise.ch>
2023-01-01 18:14:11 +0600

    drawer design added customer type

--- src/Components/CustomDrawer/CustomDrawer.js
+++ src/Components/CustomDrawer/CustomDrawer.js
@@ -1,12 +1,28 @@
-import { Drawer } from 'antd';
-import React from 'react';
+import { Drawer, Select } from 'antd';
+import React, { useState } from 'react';
+import './CustomDrawer.scss';
+const options = ['All', 'ELUX', 'B2B', 'B2C'];
 
 function CustomDrawer({ onClose, open }) {
+    const [selectedItems, setSelectedItems] = useState([]);
+    const filteredOptions = options.filter(o => !selectedItems.includes(o));
     return (
         <Drawer title="Filters" placement="right" onClose={onClose} open={open}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            <div className="filter-type-options">
+                <Select
+                    mode="tags"
+                    placeholder="Customer Type"
+                    value={selectedItems}
+                    onChange={setSelectedItems}
+                    style={{
+                        width: '100%',
+                    }}
+                    options={filteredOptions.map(item => ({
+                        value: item,
+                        label: item,
+                    }))}
+                />
+            </div>
         </Drawer>
     );
 }
