fe725ae1dbc4a30294f5a6fe4e39fd204ae27b1f
Md Asifuzzaman Suvo <asifuzzaman.suvo@selise.ch>
2023-01-09 16:23:29 +0600

    redux dispatch added and initial value changed

--- src/Redux/Slice/GlobalFilter/typeOfDataSlice.js
+++ src/Redux/Slice/GlobalFilter/typeOfDataSlice.js
@@ -1,10 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    value: [
-        'events',
-        'participants',
-    ],
+    value: 'events',
 };
 
 export const typeOfDataSlice = createSlice({
